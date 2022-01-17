296ebaae82cd981f150e2102fe0416ce5d00035f
dbeez <63750556+dbeez@users.noreply.github.com>
2022-01-17 10:30:20 -0500

    updating all files

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -1,25 +1,15 @@
+<?php
 get_header(); ?>
 
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+
+				<article class="case-study">
+				   <?php the_content(); ?>
+			    </article>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
-
-	</div><!-- #primary -->
-
-<?php get_footer(); ?>
-
-get_header(); ?>
-
-	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<h1>This is my case studies page</h1>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content --> 
 
 	</div><!-- #primary -->
 
